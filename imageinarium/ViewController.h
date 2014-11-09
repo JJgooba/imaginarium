da450906c1682ff2def99362705a65ae42bc6ebe
JJ <github@buzhi.net>
2014-11-10 05:54:24 +0700

    Initial Commit

---
+++ imageinarium/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imageinarium
+//
+//  Created by JJ on 11/10/14.
+//  Copyright (c) 2014 JJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
